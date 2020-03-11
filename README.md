cf90a22d4d688b77a926f655b7e7a948653ac637
barkas62 <barkas62@gmail.com>
2020-03-11 14:10:31 -0700

    feat: Web app finished, plus some refactoring. Web app: all functionality. Refactoring: loading data files from correct folders.

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# Disaster_Response_Pipelines
-Project #2 in Data Scientist Udacity Nanodegree 
+# Disaster Response Pipeline Project
+
+
+### Project structure:
+```
+- app
+| - template
+| |- master.html  # index page template
+| |- go.html      # classification results page
+|- run.py         # Flask app
+
+- data
+|- disaster_categories.csv  # data to process
+|- disaster_messages.csv    # data to process
+|- process_data.py          # script implementing ETL pipeline
+|- DisasterResponse.db      # database to save clean data to
+
+- models
+|- train_classifier.py      # script implementing ML pipeline
+|- disaster_model.pkl       # pickled classification model
+
+- dev
+|- ETL Pipeline Preparation.ipynb # etl exploration
+|- ML Pipeline Preparation.ipynb  # ML exploration
+
+- README.md
+```
+
+### Required packages:
+
+- flask
+- joblib
+- jupyter
+- pandas
+- plot.ly
+- numpy
+- scikit-learn
+- sqlalchemy
+- nltk
+
+### Instructions:
+1. Run the scripts in the project's root directory to prepare data and train the model:
+
+    - ETL pipeline to cleans data and store results in database:
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - ML pipeline to train classifier and save the model:
+        `python models/train_classifier.py data/DisasterResponse.db models/disaster_model.pkl`
+
+2. Run the script in the app's directory to start web app locally:
+    `python run.py`
+
+3. In any browser, go to http://127.0.0.1:3001/
+
+
+
+### Data and ETL Pipeline details:
+
+The data in this project comes from a modified version of the figure-eight [disaster response data](https://www.figure-eight.com/dataset/combined-disaster-response-data/).
+They contain short messages labelled as one or several of 36 different categories related to disaster response.
+
+_Extraction_:
+Data are read from `data/disaster_messages.csv` and `data/disaster_categories.csv` files into pandas DataFrames, then merged into one DataFrame
+_Transformation_:
+Category labels are converted from texts into `0/1` encoding, duplicated rows are removed.
+Below is the bar plot of distribution of messages by categories.
+![ ](img/categories_distribution.png)
+We can see that for most categories we have a very unbalanced distribution with too few positive examples.
+_Loading_:
+Cleaned data are saved into `sqlight` database.
+
+### ML Pipeline details:
+_Vectorization:_
+Message texts are tokenized:
+- text normalized (de-capitalized, only letters and digits left);
+- split into words;
+- stop words removed;
+- remaining words are lemmatized and stemmed.
+
+Lists of tokens are converted in vectors using TF-IDF vectorizer.
+
+_Model Training:_
+Data set is splitted into **Train** and **Test** subsets. Train subset is used to train a multi-label classifier. Initially, the **Random Forest** classifier with  was tried, the hyperparameters selection was done by `GridSearchCV`. The fitted model was scored on both **Train** and **Test** subsets.
+It appears that this approach leads to:
+- Severe overfitting (average accuracy 0.25 on Test vs 0.87 on Train data);
+- Strong disbalance between scores for positive and negative examples (due to training data structure: too few positive samples for many categories).
+
+As an illustration of the second statement, below is the bar graph of recall scores for all categories: positive (red bars) vs negative (blue bars) samples
+![ ](img/recall.png)
+
+To alleviate the problem of overfitting, the number of features was reduced by using **Truncated SVD**. Several attempts showed that **KNN Classifier** works best with these truncated features set.
+
+The further improvement may include the weighting of positive/negative samples to address under-representation of positive samples.
+
+### Web Application.
+
+On main page it shows an overview of Training Dataset:
+- Message Text Length Distribution;
+- Percentage of Messages in English vs Translated Messages;
+- Distribution of Messages by Categories.
+
+Also it provides the UI to input a sample message and apply the trained model to get the classification results.
+
+
+
+
--- app/run.py
+++ app/run.py
@@ -1,75 +1,130 @@
 import json
+import joblib
+import numpy as np
 import plotly
 import pandas as pd
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+import re
+import nltk
+#from nltk.stem import WordNetLemmatizer
+#from nltk.tokenize import word_tokenize
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Pie
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+    # Normalize text: leave decapitalized letters and digits only
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    # Split into words
+    words = text.split()
 
-    return clean_tokens
+    # Remove stop words
+    words = [w for w in words if w not in stopwords.words("english")]
+
+    # Reduce words to their root form
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in words]
+
+    # Reduce words to their stems
+    stemmed = [PorterStemmer().stem(w) for w in lemmed]
+
+    return stemmed
+
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('labeled_messages', engine)
+engine.dispose()
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/disaster_model.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
+
+    lengths = df.message.str.split().str.len()
+    length_counts, length_division = np.histogram(lengths,
+                                              range=(0, lengths.quantile(0.99)))
+
+    translated_names = ['Translated Messages', 'Messages in English']
+    translated_counts = [df['original'].notnull().sum(), df['original'].isnull().sum()]
+
+    category_names = df.columns[4:].tolist()
+    category_counts = df[category_names].sum().sort_values(ascending=False)
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
                 Bar(
-                    x=genre_names,
-                    y=genre_counts
+                    x=length_division,
+                    y=length_counts
+                    )
+            ],
+
+            'layout': {
+                'title': 'Message Length Distribution',
+                },
+            'yaxis': {
+                'title': "Count"
+            },
+            'xaxis': {
+                'title': "Message Length"
+            }
+        },
+        {
+            'data': [
+                Pie(
+                    labels=translated_names,
+                    values=translated_counts
                 )
             ],
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Percentage of Messages in English and Translated',
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Message Count"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Category Name"
                 }
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +133,16 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
+
+    print( classification_labels )
+
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
